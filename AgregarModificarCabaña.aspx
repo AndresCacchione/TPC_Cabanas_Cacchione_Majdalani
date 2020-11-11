c9ed12de9412d453e415614f8e819a6fa8377932
Jero <jeromajdalani@hotmail.com>
2020-11-11 19:11:03 -0300

    Agregar Cabañas Visual

---
+++ AgregarModificarCabaña.aspx
@@ -0,0 +1,52 @@
+﻿<%@ Page Title="" Language="C#" MasterPageFile="~/Site.Master" AutoEventWireup="true" CodeBehind="AgregarModificarCabaña.aspx.cs" Inherits="TPC_CacchioneMajdalani.AgregarModificarCabaña" %>
+<asp:Content ID="Content1" ContentPlaceHolderID="MainContent" runat="server">
+     
+    <form>
+  <div class="form-row">
+      <div class="form-group col-md-6">
+    <label for="inputPrecioDiario">Precio por dia</label>
+    <input type="number" class="form-control" id="PrecioDiario" placeholder="$ 1.000.000.000" required runat="server">
+     </div>
+              <div class="form-group col-md-6">
+    <label for="inputTiempoEntreReservas">Tiempo entre reservas (%)</label>
+    <input type="time" class="form-control" id="TiempoEntreReservas" placeholder="HH-MM-SS" required runat="server">
+  </div>
+  
+  </div>
+         <div class="form-row">
+    <div class="form-group col-md-6">
+      <label for="inputCapacidad">Capacidad</label>
+      <input type="number" class="form-control" id="Capacidad" placeholder="Cantidad de personas" required runat="server">
+    </div>
+        <div class="form-group col-md-6">
+    <label for="inputCheckIn">Hora Recepcion</label>
+    <input type="time" class="form-control" id="CheckIn" placeholder="HH-MM-SS" required runat="server">
+  </div>
+          </div>
+        <div class="form-row">
+  <div class="form-group col-md-6">
+    <label for="inputAmbientes">Ambientes</label>
+    <input type="number" class="form-control" id="Ambientes" placeholder="Cantidad de Ambientes" required runat="server">
+  </div>
+   <div class="form-group col-md-6">
+    <label for="inputCheckOut">Hora Salida</label>
+    <input type="time" class="form-control" id="CheckOut" placeholder="HH-MM-SS" required runat="server">
+  </div> 
+  </div>
+
+    <div class="form-row">
+     <div class="form-group col-md-6">
+      <label for="inputImagen">URL de la Imagen</label>
+      <input type="url" class="form-control" id="Imagen" placeholder="URL de la imagen" required runat="server">
+    </div>
+    </div>
+
+  <asp:Button class="btn btn-success mr-auto ml-auto" id="BtnAgregarCabaña" onclick="BtnAgregarCabaña_Click" Text="Agregar" runat="server" />
+  <a href="Complejos.aspx" class="btn btn-primary mr-auto ml-auto">Volver</a>
+
+
+
+
+
+
+</asp:Content>
---
+++ AgregarModificarCabaña.aspx.designer.cs
@@ -0,0 +1,89 @@
+﻿//------------------------------------------------------------------------------
+// <generado automáticamente>
+//     Este código fue generado por una herramienta.
+//
+//     Los cambios en este archivo podrían causar un comportamiento incorrecto y se perderán si
+//     se vuelve a generar el código. 
+// </generado automáticamente>
+//------------------------------------------------------------------------------
+
+namespace TPC_CacchioneMajdalani
+{
+
+
+    public partial class AgregarModificarCabaña
+    {
+
+        /// <summary>
+        /// Control PrecioDiario.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.HtmlControls.HtmlInputGenericControl PrecioDiario;
+
+        /// <summary>
+        /// Control TiempoEntreReservas.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.HtmlControls.HtmlInputGenericControl TiempoEntreReservas;
+
+        /// <summary>
+        /// Control Capacidad.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.HtmlControls.HtmlInputGenericControl Capacidad;
+
+        /// <summary>
+        /// Control CheckIn.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.HtmlControls.HtmlInputGenericControl CheckIn;
+
+        /// <summary>
+        /// Control Ambientes.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.HtmlControls.HtmlInputGenericControl Ambientes;
+
+        /// <summary>
+        /// Control CheckOut.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.HtmlControls.HtmlInputGenericControl CheckOut;
+
+        /// <summary>
+        /// Control Imagen.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.HtmlControls.HtmlInputGenericControl Imagen;
+
+        /// <summary>
+        /// Control BtnAgregarCabaña.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Button BtnAgregarCabaña;
+    }
+}
